641160d9f795d58b912e65b9d7eecaec528fc598
Anders <ande842n@zbc.dk>
2025-01-06 11:17:39 +0100

    idk

---
+++ src/bin/hveps.rs
@@ -0,0 +1,9 @@
+struct Age(i32);
+struct Victims(i32);
+struct Flowers(i32);
+
+fn main() {
+    let age = Age(128);
+    let victims = Victims(4);
+    let flowers = Flowers(654);
+}
--- src/bin/opgave1.rs
+++ src/bin/opgave1.rs
@@ -4,14 +4,14 @@ fn print_pointer(p: &char) {
 }
 
 fn main() {
-    let char1 = 'a';
+    let char1 = 'a'; //location af a bliver udskrevet 2 gange, samme værdi begge gange
     let char2 = 'b';
 
     let mut p = &char1;
 
     print_pointer(p);
 
-    // Din kode her
+    let mut p = &char2;
 
     print_pointer(p);
 }
